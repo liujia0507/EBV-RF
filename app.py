5a0a69167bf69ac8c509fe0804702431d626d00f
liujia0507 <1002448420@qq.com>
2026-06-25 14:22:49 +0800

    更改颜色

--- app.py
+++ app.py
@@ -103,8 +103,8 @@ if st.button("Predict"):
 
         # 瀑布图 (waterfall plot) — 显式区分正负颜色
         # 正向贡献用红色，负向贡献用蓝色
-        shap.plots.colors.red_rgb = "#E64B35"     # 猩红色
-        shap.plots.colors.blue_rgb = "#4DBBD5"    # 道奇蓝
+        shap.plots.colors.red_rgb = "#e61772"     # 猩红色
+        shap.plots.colors.blue_rgb = "#2ea7e0"    # 道奇蓝
         shap.plots.waterfall(
             shap.Explanation(
                 values=sv[0],
